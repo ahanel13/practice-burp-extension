d53634d8477ba0e566ae9e27671c3c111f8ad6e8
Anthony Hanel <ahanel13@gmail.com>
2023-02-13 12:14:37 -0600

    Added load output

--- src/main/java/main/java/example/httphandler/HttpHandlerExample.java
+++ src/main/java/main/java/example/httphandler/HttpHandlerExample.java
@@ -2,13 +2,19 @@ package main.java.example.httphandler;
 
 import burp.api.montoya.BurpExtension;
 import burp.api.montoya.MontoyaApi;
+import burp.api.montoya.logging.Logging;
 
 public class HttpHandlerExample implements BurpExtension {
     @Override
     public void initialize(MontoyaApi api) {
         api.extension().setName("HTTP Handler Example");
 
+        Logging logger = api.logging();
+
         //register out http handler with burp.
         api.http().registerHttpHandler(new MyHttpHandler(api));
+
+        // write a message to our output stream
+        logger.logToOutput("http handler registered");
     }
 }
